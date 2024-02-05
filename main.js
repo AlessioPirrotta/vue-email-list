2bf2e93e13225603655cd4b087cd296207e0283b
AlessioPirrotta <alessiopirrotta92@gmail.com>
2024-02-05 19:04:34 +0100

    Exercise completed without bonus

---
+++ main.js
@@ -0,0 +1,27 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        rMail:'',
+        arrayMail: []
+
+      }
+    },
+    mounted() {
+        console.log('ciao')
+    },
+
+    methods: {
+        emailRandom() {
+            for (let i = 0; i < 10; i++) {
+              axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((result) => { 
+                  this.arrayMail.push(result.data.response);
+                  
+                })
+            }
+            console.log(this.arrayMail)
+        }
+    }
+  }).mount('#app')
